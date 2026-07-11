b07cee5a3fbe6e1a8ecd393d844a540cdd9e70f5
Eugene Tiah <agbezugeeugene@gmail.com>
2026-07-11 08:09:05 +0000

    Update server.js with Telegram bot and process handlers

--- src/server.js
+++ src/server.js
@@ -5,10 +5,15 @@ const env = require('./config/env');
 const logger = require('./utils/logger');
 const { startScheduler } = require('./services/schedulerService');
 const { startWhatsAppWeb } = require('./services/whatsappWebService');
+const { startTelegramBot } = require('./services/telegramService');
 
-startWhatsAppWeb();
+//startWhatsAppWeb();
 startScheduler();
+startTelegramBot();
 
-app.listen(env.port, () => {
+const server = app.listen(env.port, () => {
   logger.info(`Server running on port ${env.port}`);
 });
+
+process.on('SIGTERM', () => { server.close(); });
+process.on('SIGINT', () => { server.close(); process.exit(); });
