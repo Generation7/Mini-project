ebc75f16ccb7aac4468a224ac30e6cddf2977832
Eugene Tiah <agbezugeeugene@gmail.com>
2026-07-12 10:19:07 +0000

    Auto update

--- src/server.js
+++ src/server.js
@@ -1,19 +1,5 @@
-require('./db/init');
+logger.info('Telegram bot started');
+  return bot;
+}
 
-const app = require('./app');
-const env = require('./config/env');
-const logger = require('./utils/logger');
-const { startScheduler } = require('./services/schedulerService');
-const { startWhatsAppWeb } = require('./services/whatsappWebService');
-const { startTelegramBot } = require('./services/telegramService');
-
-//startWhatsAppWeb();
-startScheduler();
-startTelegramBot();
-
-const server = app.listen(env.port, () => {
-  logger.info(`Server running on port ${env.port}`);
-});
-
-process.on('SIGTERM', () => { server.close(); });
-process.on('SIGINT', () => { server.close(); process.exit(); });
+module.exports = { startTelegramBot };
