3736aa357a81f1237b94be77599fe93bcd67d98a
Eugene Tiah <agbezugeeugene@gmail.com>
2026-07-14 13:45:01 +0000

    Auto update

--- src/routes/lectureRoutes.js
+++ src/routes/lectureRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const lectureController = require('../controllers/lectureController');
+const { requireAuth } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-router.post('/', lectureController.createLecture);
-router.get('/', lectureController.listLectures);
-router.get('/:id', lectureController.getLecture);
-router.delete('/:id', lectureController.deleteLecture);
+router.post('/', requireAuth, lectureController.createLecture);
+router.get('/', requireAuth, lectureController.listLectures);
+router.get('/:id', requireAuth, lectureController.getLecture);
+router.delete('/:id', requireAuth, lectureController.deleteLecture);
 
 module.exports = router;
